e18b2fad30f3159292138edf733dfceb7c15b660
Md Iqbal Haider Khan <0jkk005@gmail.com>
2024-03-18 00:07:50 +0600

    Removed all Old Files

--- pointer.php
+++
@@ -1,6 +0,0 @@
-    <!-- ==== Custom Curpor Pointer ==== -->
-    <div class="mouse-follower">
-        <span class="cursor-outline"></span>
-        <span class="cursor-dot"></span>
-    </div>
-    <!-- ==== Custom Curpor Pointer ==== -->
--- preloader.php
+++
@@ -1,6 +0,0 @@
-        <!-- ==== preloader ==== -->
-        <div id="preloader">
-            <div class="layla-preloader text-center">               
-            </div>
-        </div>
-        <!-- ==== preloader ==== -->
